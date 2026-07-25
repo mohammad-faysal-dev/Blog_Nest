ccd60dcbf3761acd04b054f4966cb54d3d39ffcb
Faysal2123 <foysal2005n1@gmail.com>
2026-07-26 01:38:52 +0600

    feat(auth): configure Better Auth and auth routes

---
+++ src/lib/auth.ts
@@ -0,0 +1,33 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+// If your Prisma file is located elsewhere, you can change the path
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  trustedOrigins: [process.env.APP_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: true
+      },
+      phone: {
+        type: "string",
+        required: false
+      },
+      status: {
+        type: "string",
+        required: true,
+        defaultValue: "ACTIVE"
+      }
+    }
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+});
