c7d313fb8de45329d31a2a44daa46b7b6bc0699d
Faysal2123 <foysal2005n1@gmail.com>
2026-07-25 21:49:50 +0600

    feat: implement create post functionality

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express, { Application } from "express";
+import { postRouter } from "./modules/post/post.route";
+
 
 const app: Application = express();
+
+app.use(express.json());
+app.use("/posts", postRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 export default app;
