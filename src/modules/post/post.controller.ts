51466d1ac7583e10c9224a2970bc15a3dc75a95f
Faysal2123 <foysal2005n1@gmail.com>
2026-07-29 22:21:05 +0600

    feat: implement multi-field blog search

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -20,7 +20,9 @@ const createPost = async (req: Request, res: Response) => {
 };
 const getAllPost = async (req: Request, res: Response) => {
   try {
-    const result = await postService.getAllPost();
+    const { search } = req.query;
+    const searchString = typeof search === "string" ? search : undefined;
+    const result = await postService.getAllPost({ search: searchString });
     res.status(200).json(result);
   } catch (e) {
     res.status(400).json({
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -14,8 +14,30 @@ const createPost = async (
   return result;
 };
 
-const getAllPost = async () => {
-  const allPost = await prisma.post.findMany();
+const getAllPost = async (payload: { search?: string | undefined }) => {
+  const allPost = await prisma.post.findMany({
+    where: {
+      OR: [
+        {
+          title: {
+            contains: payload.search as string,
+            mode: "insensitive"
+          }
+        },
+        {
+          content: {
+            contains: payload.search as string,
+            mode: "insensitive"
+          }
+        },
+        {
+          tags: {
+            has: payload.search as string,
+          },
+        }
+      ]
+    }
+  });
   return allPost;
 };
 export const postService = {
