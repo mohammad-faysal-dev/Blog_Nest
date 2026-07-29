bc06f2512a75ea119de2fbe97d9da0b193e2c14b
Faysal2123 <foysal2005n1@gmail.com>
2026-07-29 21:20:19 +0600

    feat: implement getAllPost service

--- src/modules/post/post.route.ts
+++ src/modules/post/post.route.ts
@@ -3,6 +3,7 @@ import { PostController } from "./post.controller";
 import auth, { UserRole } from "../../middlewares/auth";
 
 const router = express.Router();
+router.get("/", PostController.getAllPost);
 router.post("/", auth(UserRole.USER), PostController.createPost);
 
 export const postRouter = router;
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -2,17 +2,23 @@ import { Post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async (
-  data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">,userId:string
+  data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">,
+  userId: string,
 ) => {
   const result = await prisma.post.create({
-   data:{
-     ...data,
-    authorId:userId
-   }
+    data: {
+      ...data,
+      authorId: userId,
+    },
   });
   return result;
 };
 
+const getAllPost = async () => {
+  const allPost = await prisma.post.findMany();
+  return allPost;
+};
 export const postService = {
   createPost,
+  getAllPost,
 };
